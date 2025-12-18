00e324c1e4028939e8d7d06f6a99eb115b1ce244
Vaibhav Vinil Kumar <vaibhavvinil@gmail.com>
2025-12-18 12:41:53 +0400

    image bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
+  images: {
+    domains: [
+      'pvmarket.com',
+      'www.pvmarket.com',
+      'pv.market',
+      'www.pv.market',
+      'storage.googleapis.com',
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
